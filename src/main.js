0e927809023f9c95500c5c46e09d8852053896a9
Ryland Adams <rylandadams@yahoo.com>
2022-11-28 14:27:50 -0800

    starting the process of breaking down processes and adding comments

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import Mandalorian from './character.js';
 import startGame from './game-nav.js';
 $;
 
-let characterName = "Mando";
-let selectedClass = "Ranged";
-let player = new Mandalorian (characterName, selectedClass);
+let characterName = 'Mando';
+let selectedClass = 'Ranged';
+let player = new Mandalorian(characterName, selectedClass);
 
 // export features declared earlier
-export { player }; 
+export { player };
 
+// Generates stats in a range based on class
 player.getStats();
+// Gives base equipment
 player.getStarterEquip();
+
 //module.exports.player = player;
 startGame(player);
-
